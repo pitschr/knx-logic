82acc187bf06f13f4f1a0d2e1a860cb6e5ddd5f9
pitschr <github@pitschmann.li>
2022-11-09 20:45:14 +0100

    Format and update copyrights

--- src/main/java/li/pitschmann/knx/logic/RouterInternal.java
+++ src/main/java/li/pitschmann/knx/logic/RouterInternal.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2022 Pitschmann Christoph
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package li.pitschmann.knx.logic;
 
 import li.pitschmann.knx.core.annotations.Nullable;
@@ -358,8 +375,8 @@ final class RouterInternal {
      * In case the value could not be converted a {@link RouterException} will be thrown.
      *
      * @param source the pin where the value comes from
-     * @param target   the pin that should set with the value
-     * @param value     the given value; may be null
+     * @param target the pin that should set with the value
+     * @param value  the given value; may be null
      * @return the value; may not be null
      * @throws RouterException in case the conversion was not possible
      */
@@ -397,7 +414,7 @@ final class RouterInternal {
             // Number (1, 1.0) -> Boolean
             else if (Number.class.isAssignableFrom(valueType)
                     && targetFieldType == Boolean.class) {
-                final var iValue = (int)value;
+                final var iValue = (int) value;
                 if (iValue == 0) {
                     convertedValue = Boolean.FALSE;
                 } else if (iValue == 1) {
--- src/main/java/li/pitschmann/knx/logic/event/VariableEventChannel.java
+++ src/main/java/li/pitschmann/knx/logic/event/VariableEventChannel.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2022 Pitschmann Christoph
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package li.pitschmann.knx.logic.event;
 
 import li.pitschmann.knx.core.utils.Maps;
@@ -13,6 +30,10 @@ import java.util.NoSuchElementException;
 public final class VariableEventChannel implements EventChannel {
     public static final String CHANNEL_ID = "var";
     private final Map<String, Object> variableMap = Maps.newHashMap(100);
+
+    public static EventKey createKey(final String name) {
+        return new EventKey(CHANNEL_ID, name);
+    }
 
     @Override
     public String getChannel() {
@@ -32,10 +53,6 @@ public final class VariableEventChannel implements EventChannel {
             throw new NoSuchElementException("Could not find data for variable name: " + name);
         }
         return data;
-    }
-
-    public static EventKey createKey(final String name) {
-        return new EventKey(CHANNEL_ID, name);
     }
 
     @Override
